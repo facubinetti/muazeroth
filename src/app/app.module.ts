48ce5e84d3a50514b19df467de1aee6d07633061
facubinetti <facubinetti.96@gmail.com>
2020-11-22 23:07:10 -0300

    crea componentes iniciales para el shop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MuazerothInicioComponent } from './muazeroth-inicio/muazeroth-inicio.component';
+import { MuShopComponent } from './mu-shop/mu-shop.component';
+import { ShopListComponent } from './shop-list/shop-list.component';
+import { CartShopComponent } from './cart-shop/cart-shop.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MuazerothInicioComponent
+    MuazerothInicioComponent,
+    MuShopComponent,
+    ShopListComponent,
+    CartShopComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mu-shop/mu-shop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mu-shop',
+  templateUrl: './mu-shop.component.html',
+  styleUrls: ['./mu-shop.component.scss']
+})
+export class MuShopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

ff2a3059ffe01c0066c0092bee1b325099838461
facubinetti <facubinetti.96@gmail.com>
2020-11-22 23:05:32 -0300

    agrega inicio y su respectivo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MuazerothInicioComponent } from './muazeroth-inicio/muazeroth-inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MuazerothInicioComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/muazeroth-inicio/muazeroth-inicio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-muazeroth-inicio',
+  templateUrl: './muazeroth-inicio.component.html',
+  styleUrls: ['./muazeroth-inicio.component.scss']
+})
+export class MuazerothInicioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

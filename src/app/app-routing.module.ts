6d0f3c3aa0818ac0ff177aea359a14c890daaac2
facubinetti <facubinetti.96@gmail.com>
2020-11-23 20:45:11 -0300

    agregado router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MuShopComponent } from './mu-shop/mu-shop.component';
+import { MuazerothInicioComponent } from './muazeroth-inicio/muazeroth-inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component:MuazerothInicioComponent},
+  {path:'home',component:MuazerothInicioComponent},
+  {path:'mushop',component:MuShopComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
